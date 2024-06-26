35d0e4f3786a3fd051968747ddd7d30c0c199da5
2602hoang <vuhuyhoangboj@gmail.com>
2024-06-26 13:28:48 +0700

    v3

--- src/url/url.js
+++ src/url/url.js
@@ -1,4 +1,4 @@
-export const URL = 'https://api-mvc-sql-1.onrender.com'
+export const URL = 'https://api-mvc-sql1.vercel.app'
 // 'https://api-mvc-sql-1.onrender.com';
 //  'http://localhost:8080',
    
